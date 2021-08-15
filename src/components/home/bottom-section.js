c6d89fabb390704655f7078c2ec9e2533e497fe9
hafsah <emekomahafsah@gmail.com>
2021-08-15 21:17:37 +0100

    modifications to  page styles

--- src/components/home/bottom-section.js
+++ src/components/home/bottom-section.js
@@ -16,9 +16,9 @@ const BottomSection = () => {
         <section className={sectionStyle}>
             <div className="">
                 <Logo/>
-                <p className="mt-[7px] mb-[24px]">Project by <a href="https://lulunwenyi.com">Lulu Nwenyi</a> </p>
+                <p className="mt-[7px] mb-[24px] text-[#3D3D3D] ">Project by <a href="https://lulunwenyi.com">Lulu Nwenyi</a> </p>
                 <p className="text-[17px] w-[618px]">
-                    Building this project was an amazing learning experience for me as its designer and backend developer. I also want to acknowledge my friend <a href="/something">Hafsah</a> who contributed to this project by building the frontend.  To give feedback or ideas/contributions, please find me on Twitter and if you like this project or find it useful, you can <a href="https://buymeacoffee.com/lulunwenyi">buy me a coffee. 😊</a> 
+                    Building this project was an amazing learning experience for me as its designer and backend developer. I also want to acknowledge my friend <a href="https://twitter.com/hafsyforever">Hafsah</a> who contributed to this project by building the frontend.  To give feedback or ideas/contributions, please find me on <a href="https://twitter.com/lulunwenyi">Twitter </a> and if you like this project or find it useful, you can <a href="https://buymeacoffee.com/lulunwenyi">buy me a coffee. 😊</a> 
                 </p>
             </div>
 
