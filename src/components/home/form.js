2daccd116fb4c7c25036cf12a5332154803de84e
hafsah <emekomahafsah@gmail.com>
2021-08-21 21:14:25 +0100

    Feat: posting affirmations to the API

--- src/components/home/form.js
+++ src/components/home/form.js
@@ -22,7 +22,7 @@ const AffirmationForm = () => {
             setTimeout(function(){
                 setLoading(false)
                 setAffirmationEntered(true)
-            }, 3000)
+            }, 1000)
         })
        
     }
